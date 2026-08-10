04130a612fe8df1eaaeab5a87dce132b30d16889
Ali-Aahmed <Ali.ahmed@mu.edu.eg>
2026-08-11 00:00:30 +0300

    feat: configure SQL Server and EF Core

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MedicalCenterSystem.Data;
+
 namespace MedicalCenterSystem
 {
     public class Program
@@ -8,6 +11,10 @@ namespace MedicalCenterSystem
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer( 
+                builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
